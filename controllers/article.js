81d6e5bcbbfc8eb027ccb2aee2729396b4c1b427
morgan <morgan>
2022-10-18 14:45:17 +0700

    create comment

--- controllers/article.js
+++ controllers/article.js
@@ -383,7 +383,7 @@ module.exports = {
                  ArticleComments
                   .create({
                       article_id            : data.id,
-                      parent_id             : '',
+                      parent_id             : 0,
                       user_id               : req.userId,
                       comment               : req.body.comment,
                       status                : 'active',
@@ -477,7 +477,7 @@ module.exports = {
                       message : 'Success',
                       data    : {
                         user,
-                        comment_id          : comments.id, 
+                        comment_id          : data.id, 
                         created_at          : data.created_at,
                         comment             : data.comment,
                         is_like             : false,
@@ -485,6 +485,7 @@ module.exports = {
                         total_comment_reply : data.total_comment_reply,
                         comment_replies     :{
                           user,
+                          comment_reply_id    : comments.id,
                           comment             : comments.comment,
                           is_like             : false,
                           total_comment_like  : comments.total_comment_like,
